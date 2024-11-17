bc20bb09a6978695b23c813e6494b61f5ecf248f
Aage Dahl <age.dahl@external.technipfmc.com>
2024-11-17 19:47:00 +0100

    Fetching data from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,63 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useEffect, useState } from "react";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [greeting, setGreeting] = useState("Hello from Vite!");
+  const [picture, setPicture] = useState(viteLogo);
+  const [joke, setJoke] = useState("");
+  const [quote, setQuote] = useState("");
+  const [age, setAge] = useState(undefined as number | undefined);
+
+  useEffect(() => {
+    if (count !== 0) {
+      fetch("/api/profile/greetings?name=Ola")
+        .then((res) => res.json())
+        .then((data) => setGreeting(data.greeting));
+      fetch("/api/profile/profile-picture")
+        .then((res) => res.json())
+        .then((data) => setPicture(data.message));
+      fetch("/api/profile/joke")
+        .then((res) => res.json())
+        .then((data) => setJoke(data.value));
+      fetch("/api/profile/quote?category=happiness")
+        .then((res) => res.json())
+        .then((data) => setQuote(data.quote));
+      fetch("/api/profile/age?name=Ola")
+        .then((res) => res.json())
+        .then((data) => setAge(data.age));
+    }
+  }, [count]);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href={picture} target="_blank">
+          <img src={picture} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <div>
+        <h1>{greeting}</h1>
+        {age && <p>Guessed age: {age}</p>}
+      </div>
+      {joke && (
+        <div className="card">
+          <h2>Joke of the day</h2>
+          <p>{joke}</p>
+        </div>
+      )}
+      {quote && (
+        <div className="card">
+          <h2>Quote of the day</h2>
+          <p>{quote}</p>
+        </div>
+      )}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          update count {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  server: {
+    proxy: {
+      "/api": "http://localhost:8080",
+    },
+  },
+  build: { outDir: "../src/main/resources/static" },
+}) satisfies UserConfig;
